1c8cea290fac92a3495ace4c51b986655f1dfa7a
Zaki Indra <indra.zaki@gmail.com>
2024-09-23 19:20:14 -0400

    add booking api

---
+++ api/src/main/java/com/cinema/cinema/controller/BookingController.java
@@ -0,0 +1,38 @@
+package com.cinema.cinema.controller;
+
+import com.cinema.cinema.model.Booking;
+import com.cinema.cinema.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Booking> getBookingById(@PathVariable Long id) {
+        Booking booking = bookingService.getBookingById(id);
+        return ResponseEntity.ok(booking);
+    }
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingService.createBooking(booking);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Booking> updateBooking(@PathVariable Long id, @RequestBody Booking bookingDetails) {
+        Booking updatedBooking = bookingService.updateBooking(id, bookingDetails);
+        return ResponseEntity.ok(updatedBooking);
+    }
+
+}
