d9c950d5527b9baa522896d3e7ba7b8dbdc1f875
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2024-10-23 13:23:16 -0400

    Implement adding new credit cards

--- app/static/js/payment-methods/forms.js
+++ app/static/js/payment-methods/forms.js
@@ -3,6 +3,21 @@ function handleNewPaymentMethod(event) {
 
     const form = event.target;  // 'event.target' refers to the form element
     const formData = new FormData(form);
+    const formDataObj = Object.fromEntries(formData.entries());
 
-    fetch()
+    fetch(
+        'http://localhost:8080/customer/1/creditCard', 
+        {
+            method: 'POST',
+            body: JSON.stringify(formDataObj),
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            }
+        }
+    ).then((data) => {
+        console.log(data)
+    }).catch((e) => {
+        console.log(e)
+    });
 }
