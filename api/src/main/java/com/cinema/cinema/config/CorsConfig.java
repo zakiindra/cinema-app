fa056903caf41b9bdb778a3a79543e727ec73c9f
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2024-10-23 11:57:35 -0400

    Connect payment methods list to DB

--- api/src/main/java/com/cinema/cinema/config/CorsConfig.java
+++ api/src/main/java/com/cinema/cinema/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:[*]")
+                        .allowedOriginPatterns("*")  // http://localhost:[*]
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
