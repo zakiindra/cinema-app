b17a5066be9f74f264e55bc36c4acfee33f0ff9f
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2024-10-23 11:57:35 -0400

    Connect payment methods list to DB

---
+++ app/static/js/payment-methods/forms.js
@@ -0,0 +1,8 @@
+function handleNewPaymentMethod(event) {
+    event.preventDefault()
+
+    const form = event.target;  // 'event.target' refers to the form element
+    const formData = new FormData(form);
+
+    fetch()
+}
