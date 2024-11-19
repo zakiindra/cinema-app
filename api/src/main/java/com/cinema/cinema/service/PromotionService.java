4356bf6db6a46d28b3ff6f0eed1f0df7940c648a
Zaki Indra <indra.zaki@gmail.com>
2024-11-18 20:10:32 -0500

    add edit delete promo

--- api/src/main/java/com/cinema/cinema/service/PromotionService.java
+++ api/src/main/java/com/cinema/cinema/service/PromotionService.java
@@ -1,9 +1,11 @@
 package com.cinema.cinema.service;
 
+import com.cinema.cinema.exception.ResourceNotFoundException;
 import com.cinema.cinema.model.Promotion;
 import com.cinema.cinema.repository.PromotionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,8 +19,31 @@ public class PromotionService {
         return promotionRepository.findAll();
     }
 
+    public Promotion getPromotionById(Long id) throws ResourceNotFoundException {
+        return promotionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Promotion not found"));
+    }
+
     public Promotion addPromotion(Promotion promotion) {
         return promotionRepository.save(promotion);
+    }
+
+    @Transactional
+    public Promotion editPromotion(Long id, Promotion promotion) throws ResourceNotFoundException {
+        Promotion existingPromotion = getPromotionById(id);
+
+        existingPromotion.setPromotionValue(promotion.getPromotionValue());
+        existingPromotion.setDescription(promotion.getDescription());
+        existingPromotion.setStartTime(promotion.getStartTime());
+        existingPromotion.setEndTime(promotion.getEndTime());
+
+        promotionRepository.save(existingPromotion);
+
+        return existingPromotion;
+    }
+
+    public void deletePromotion(Long id) throws ResourceNotFoundException {
+        Promotion promotion = getPromotionById(id);
+        promotionRepository.delete(promotion);
     }
 
 }
