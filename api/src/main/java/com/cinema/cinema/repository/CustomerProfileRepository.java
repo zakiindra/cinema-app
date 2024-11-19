ab3688992ce5efa0b75cf66e70afe0e2bc7d62aa
Zaki Indra <indra.zaki@gmail.com>
2024-11-19 05:53:16 -0500

    add send promo email

--- api/src/main/java/com/cinema/cinema/repository/CustomerProfileRepository.java
+++ api/src/main/java/com/cinema/cinema/repository/CustomerProfileRepository.java
@@ -3,10 +3,12 @@ package com.cinema.cinema.repository;
 import com.cinema.cinema.model.CustomerProfile;
 import com.cinema.cinema.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 import java.util.Optional;
 
 public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
 
     Optional<CustomerProfile> findByUser(User user);
+    List<CustomerProfile> findBySubscribePromoTrue();
 }
